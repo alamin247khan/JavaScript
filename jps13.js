2c4625f97f89c73b715740239901786e1020ae81
Al Amin Khan <167293333+alamin247khan@users.noreply.github.com>
2024-10-09 19:27:38 -0600

    update

---
+++ jps13.js
@@ -0,0 +1,27 @@
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+function loadData() {
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
+
+function loadData() {
+    const url = 'http://jsonplaeholder.typicode.com/todo/1';
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))   
+}
+
+function loadData2(){
+    fetch('http://jsonplaeholder.typicode.com/todo/1')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
